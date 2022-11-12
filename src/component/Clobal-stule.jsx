545a7e5456a9671cf5ac80f9290182af17ef7f29
Maks Borenkov <borenkovmv@gmail.com>
2022-11-12 16:10:25 +0200

    two commit

---
+++ src/component/Clobal-stule.jsx
@@ -0,0 +1,7 @@
+// import { createGlobalStyle } from 'styled-components';
+// import 'normalize.css';
+// import styled from 'styled-components';
+
+// export const GlobalStyle = createGlobalStyle`
+
+// `;
---
+++ src/component/TextTutle.jsx
@@ -0,0 +1,9 @@
+// import styled from 'styled-component';
+
+// const Title = styled.h1`
+//   margin-bottom: 40px;
+// `;
+
+// export const PageTitle = ({ children }) => {
+//   return <Title>{children}</Title>;
+// };
